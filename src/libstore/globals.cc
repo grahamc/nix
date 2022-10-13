4de92447f920993398ee41bece5430d415ee72e5
Graham Christensen <graham@grahamc.com>
2022-10-13 09:34:51 -0400

    x86_64-darwin can't use Rosetta to run aarch64 binaries
    Users on an aarch64 Mac should install an aarch64 copy of Nix.
    If they'd like to use an x86_64 Nix on an aarch64 Mac and still get the
    extra-platforms support, they should configure it manually instead of
    incorrectly configuring x86_64 Macs.

--- src/libstore/globals.cc
+++ src/libstore/globals.cc
@@ -149,9 +149,7 @@ StringSet Settings::getDefaultExtraPlatforms()
     // x86_64 in aarch64 environments or vice versa since they can
     // always exec with their own binary preferences.
     if (pathExists("/Library/Apple/System/Library/LaunchDaemons/com.apple.oahd.plist")) {
-        if (std::string{SYSTEM} == "x86_64-darwin")
-            extraPlatforms.insert("aarch64-darwin");
-        else if (std::string{SYSTEM} == "aarch64-darwin")
+        if (std::string{SYSTEM} == "aarch64-darwin")
             extraPlatforms.insert("x86_64-darwin");
     }
 #endif
